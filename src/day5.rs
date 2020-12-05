7cde70ba0ca230fe43555d915d320fe60fec17e2
Delthii <32238382+Delthii@users.noreply.github.com>
2020-12-05 17:10:54 +0100

    day5

---
+++ src/day5.rs
@@ -0,0 +1,45 @@
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
+use std::collections::HashSet;
+
+pub fn solution(){
+    let reader = BufReader::new(File::open("./res/day5.in").expect("Couldn't open file"));
+    let rows: Vec<String> = reader.lines().map(|l| l.expect("Expected line")).collect();
+
+    let res = rows.iter()
+        .map(|s| s.chars().map(|c| make_binary(c))
+        .collect::<String>()).map(|s| make_tuple(&s))
+        .map(|(row, col)| row*8 + col)
+        .max()
+        .unwrap();
+
+    println!("{}", res);
+}
+
+fn make_binary(s: char) -> char{
+    match s{
+        'F' | 'L' => '0',
+        'B' | 'R' => '1',
+        _ => panic!("")
+    }
+}
+
+fn make_tuple(s: &String) -> (isize, isize){
+    (isize::from_str_radix(&s[..7], 2).unwrap(), isize::from_str_radix(&s[7..], 2).unwrap())
+}
+
+pub fn solution2(){
+    let reader = BufReader::new(File::open("./res/day5.in").expect("Couldn't open file"));
+    let rows: Vec<String> = reader.lines().map(|l| l.expect("Expected line")).collect();
+    let res: HashSet<_> = rows.iter()
+        .map(|s| s.chars().map(|c| make_binary(c))
+        .collect::<String>()).map(|s| make_tuple(&s))
+        .collect();
+    for row in 2..110{
+        for col in 0..8{
+            if !res.contains(&(row, col)){
+                println!("{}", row*8+col);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-mod day3;
+mod day5;
 use std::time::{Instant};
 
 fn main() {
     let now = Instant::now();
 
-    day3::solution();
+    day5::solution();
     println!("{}ms", now.elapsed().as_millis());
     let now = Instant::now();
 
-    day3::solution2();
+    day5::solution2();
     println!("{}ms", now.elapsed().as_millis());
 }
